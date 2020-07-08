fa18b6946da0fab561035cf506ffcb7baf96bd75
thompiler <thom@airbrake.io>
2020-07-08 15:25:45 -0700

    Add Airbrake to test PR #104
    Install Airbrake's PHPbrake, branch: update-terms
    
    https://github.com/airbrake/phpbrake/pull/104

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 This is a repo for hello world stuff.
 
 This is the composer branch.
+
+## Step 1
+
+Replace the project credentials:
+https://github.com/thompiler/greetings/blob/composer/src/helloworld/greetings.php#L12-L13
+
+Test to see if variables are filtered out once the error is in Airbrake:
+https://github.com/thompiler/greetings/blob/composer/src/helloworld/greetings.php#L26-L27
+
+Both `keysBlocklist` and the old, deprecated `keysBlacklist` option should be
+supported
+
+## Step 2
+
+To run it, use:
+
+
+```
+composer hello
+```
+
+## Step 3
+
+Check the "context" tab in Airbrake to see if the variables are filtered
+properly
--- src/helloworld/greetings.php
+++ src/helloworld/greetings.php
@@ -2,9 +2,32 @@
 namespace HelloWorld;
 
 use PHP_Timer;
+use Airbrake;
+use Exception;
 
 class Greetings {
   public static function sayHelloWorld() {
+
+    $notifier = new Airbrake\Notifier(array(
+      'projectId' => 280381,
+      'projectKey' => '70b2263cc1347a0d69e4658995d7a3e4',
+      'keysBlocklist' => ['/thingo/i', '/bingo/i'],
+    ));
+
+    Airbrake\Instance::set($notifier);
+
+    $handler = new Airbrake\ErrorHandler($notifier);
+    $handler->register();
+
+    try {
+      throw new Exception('hello from phpbrake CDCDC');
+    } catch(Exception $e) {
+      $notice = $notifier->buildNotice($e);
+      $notice['context']['thingo'] = 'please filter me';
+      $notice['context']['bingo'] = 'please filter this too';
+      $notifier->sendNotice($notice);
+    }
+
     $timer = new PHP_Timer();
     $timer->start();
     return "Hello World\n" . $timer->resourceUsage() . "\n";
